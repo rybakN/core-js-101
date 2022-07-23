7598f6e9203767b20d51240cc917c1b7aecb1892
rybakN <dnb4949@gmail.com>
2022-07-24 00:28:35 +0300

    feat: 71 task passing

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -169,8 +169,9 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let start = startFrom;
+  function getID(start) {}
 }
 
 
